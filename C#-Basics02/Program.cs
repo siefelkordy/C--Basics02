b54afec152c1278b070a6e35a919379bce2d4f61
siefelkordy <seifemadelkordy@gmail.com>
2026-08-29 02:22:11 +0300

    1st  answer

--- C#-Basics02/Program.cs
+++ C#-Basics02/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace C__Basics02
+﻿class Book
+{ 
+    public string title;
+    public int pages;
+}
+
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        //Answer to question 1
+        Book book1 = new Book();
+        object obj = book1;
+        ((Book)obj).title = "C# Basics";
+        ((Book)obj).pages = 200;
+        Console.WriteLine($"Title: {((Book)obj).title}, Pages: {((Book)obj).pages}");
+
     }
 }
+
+
